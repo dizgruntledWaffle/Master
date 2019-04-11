3d5fdab2982c3b8c0f697f91c3e44112c11eca6c
Dwaffles <dizgruntled.waffle@gmail.com>
2019-04-11 11:50:48 -0400

    did stuff

---
+++ JS/main.js
@@ -0,0 +1,30 @@
+
+
+
+var cover = $("cover");
+var speechBubble = $("speechBubble");
+
+// The the user hovers the mouse over
+// the rectangle the "expand-bounce"
+//animation is called and the duration
+// is set to 250 miliseconds.
+// When the user moves the mouse away
+// the "shrink" animation gets called
+// and the duration is set to 100
+// miliseconds.
+cover.hover(
+  function() {
+    speechBubble.css({
+      "animation-name": "expand-bounce",
+      "animation-duration": "0.25s"
+    });
+  },
+  function() {
+    speechBubble.css({
+      "animation-name": "shrink",
+      "animation-duration": "0.1s"
+    });
+  }
+);
+
+
